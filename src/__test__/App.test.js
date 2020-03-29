f243fdb0399e54ac5f3eea9dff99dab8e325ac28
schazbot <charlotte.pearce1984@gmail.com>
2020-03-29 18:06:23 +0100

    button render test passing

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,19 +1,11 @@
 import React from "react";
 import {render, cleanup} from "@testing-library/react";
-import App from "../App";
+import Login from "../pages/Login";
 
 afterEach(cleanup)
 
 it("displays the login button", () => { 
-    const { getByText } = render(<App />)
+    const { getByText } = render(<Login />)
     const loginButtonElement = getByText(/login/i)
     expect(loginButtonElement).toBeInTheDocument()
 })
-
-
-
-// test('renders learn react link', () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
